cf64cd3359f8f94b32160e334c8d3bb9c75d23fe
John Pena <jpena91@gmail.com>
2019-03-09 20:24:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -59,9 +59,9 @@ def organize_schools(schools)
     location = location_hash[:location]
     if organized_schools[location]
       organized_schools[location] << name
-      binding.pry
     else
       organized_schools[location] = []
+      binding.pry
       organized_schools[location] << name
     end
   end
